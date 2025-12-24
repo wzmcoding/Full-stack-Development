598ec4e9bd17b41d73bdd7e5d26992f230f0210c
happyWang <wangzhengmin@sinobest.cn>
2025-12-24 09:20:39 +0800

    feat(webpack): 添加多页面入口和模块解析规则
    - 配置多个页面入口文件 entry.page1 和 entry.page2
    - 添加 Vue 文件解析规则
    - 配置 JS 文件的 babel-loader 处理
    - 添加图片资源的 url-loader 处理
    - 配置 CSS 和 Less 样式文件处理规则
    - 添加字体文件的 file-loader 处理
    - 设置产物输出路径和文件命名规则
    - 配置模块解析扩展名和别名映射

--- app/webpack/config/webpack.base.js
+++ app/webpack/config/webpack.base.js
@@ -1,18 +1,69 @@
+const path = require('path');
 /**
  * webpack 基础配置
  */
 module.exports = {
     // 入口配置
     entry: {
+        'entry.page1': './app/pages/page1/entry.page1.js',
+        'entry.page2': './app/pages/page2/entry.page2.js'
     },
     // 模块解析配置（决定了要加载哪些模块，以及用什么样的方式去解析）
     module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: {
+                    loader: 'vue-loader',
+                }
+            },
+            {
+                test: /\.js$/,
+                include: [
+                    // 只对业务代码进行 babel, 加快 webpack 打包速度
+                    path.resolve(process.cwd(), './app/pages'),
+                ],
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif)(\?.+)?$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 300,
+                        esModule: false,
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+                use: 'file-loader'
+            }
+        ]
     },
     // 产物输出路径
     output: {
+        filename: 'js/[name]_[chunkhash:8].bundle.js',
+        path: path.join(process.cwd(), "./app/public/dist/prod"),
+        publicPath: '/dist/prod',
+        crossOriginLoading: 'anonymous',
     },
     // 配置模块解析的具体行为(找到具体的路径)
     resolve: {
+        extensions: ['.js', '.vue', '.less', '.css'],
+        alias: {
+            $pages: path.resolve(process.cwd(), './app/pages'),
+        },
     },
     // 配置 webpack 插件
     plugins: [],
