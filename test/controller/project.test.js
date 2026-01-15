df3c21a5abd3962004314ba82f17573dd745bd86
happyWang <wangzhengmin@sinobest.cn>
2026-01-15 18:08:34 +0800

    feat(tests): 增强项目接口测试，添加模型和项目字段验证

--- test/controller/project.test.js
+++ test/controller/project.test.js
@@ -1,22 +1,42 @@
-// const assert = require('assert');
-// const supertest = require('supertest');
-// const md5 = require('md5');
-// const elpisCore = require('../../elpis-core');
+const assert = require('assert');
+const supertest = require('supertest');
+const md5 = require('md5');
+const elpisCore = require('../../elpis-core');
 
-// const signKey = 'happyWang12385398593583958395835';
-// const st = Date.now()
+const signKey = 'happyWang12385398593583958395835';
+const st = Date.now()
 
-// describe('测试 project 相关接口', function () {
-//     this.timeout(60000);
+describe('测试 project 相关接口', function () {
+    this.timeout(60000);
 
-//     let request;
+    let request;
 
-//     it('启动服务', async () => {
-//         const app = await elpisCore.start();
-//         request = supertest(app.listen())
-//     })
+    it('启动服务', async () => {
+        const app = await elpisCore.start();
+        request = supertest(app.listen())
+    })
 
-//     it('Get /api/project/model_list', async () => {
+    it('Get /api/project/model_list', async () => {
+        let tmpRequest = request.get('/api/project/model_list');
+        tmpRequest = tmpRequest.set('s_t', st);
+        tmpRequest = tmpRequest.set('s_sign', md5(`${signKey}_${st}`));
+        const res = await tmpRequest;
 
-//     })
-// });
+        assert(res.body.success === true);
+        console.log(JSON.stringify(res.body.data, null, 2));
+        const resData = res.body.data;
+        assert(resData.length > 0);
+
+        for (let i = 0; i < resData.length; ++i) {
+            const item = resData[i];
+            assert(item.model);
+            assert(item.model.key);
+            assert(item.model.name);
+            assert(item.project);
+            for (const projKey in item.project) {
+                assert(item.project[projKey].key);
+                assert(item.project[projKey].name);
+            }
+        }
+    })
+});
